530f0dd14f256fd661d5838ae8a93de300fb537c
barnab2003 <barnabbarman@gmail.com>
2025-03-24 19:24:18 +0530

    finishing touch

--- lesson3/routes/userRoutes.js
+++ lesson3/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
-
+const app = express();
+router.use((req, res, next) => {
+  console.log("Hello from route middleware in userRoutes.js");
+  next();
+});
 
 router.get("/profile", (req, res) => {
   res.send("User Profile");
--- lesson3/server3.js
+++ lesson3/server3.js
@@ -12,7 +12,8 @@ app.use((req, res, next) => {
   });
 */
 app.use((req, res, next)  => {
-    console.log(req.headers);
+    //console.log(req.headers);
+    req.barnab= "loves Leena"; //could be accessed anywhere
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
     
@@ -22,13 +23,25 @@ app.use((req, res, next) => {
     next(); 
   });
 
-
+const userRoutes = require("./routes/userRoutes");
+  app.use("/user", userRoutes);
 app.get("/", (req, res) => {
   res.send("Hello, Express.js!");
 });
 
+app.get("/about", (req, res) => {
+  res.send("Barnab"+ req.barnab);//access req from middleware
+});
 
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+/* An Express application can use the following types of middleware:
+
+Application-level middleware
+Router-level middleware
+Error-handling middleware
+Built-in middleware
+Third-party middleware*/
